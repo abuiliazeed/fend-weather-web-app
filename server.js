28b93be719a77e811b32bef93b621f56e8330c6a
abuiliazeed <abuiliazeed@gmail.com>
2022-01-13 20:17:16 +0200

    solves #5, solves #6 , solves #7 , solves #8, solves #9 , solves #10 , solves #11 , solves #12

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
-const apiKey = 'f72063edaa945717be8af75412558d92&units=imperial';
 
 
 // Require Express to run server and routes
@@ -24,9 +23,37 @@ app.use(express.static('website'));
 
 
 // Setup Server
-const port = 8001;
+const port = 8013;
+// Notice that i added process.env.PORT with or Port in order for this code to work on Heroku
 const server = app.listen(process.env.PORT || port, listening);
 function listening(){
     console.log('the server is running');
     console.log(`running on localhost http://127.0.0.1:${port}`)
 }
+
+// we will create an arrow function get data 
+const getData = (req, res) => {
+    res.send(projectData);
+  };
+  
+  app.get("/projectData", getData);
+  // creating post data arrow function that store the retrieved data newWeather to the projectData object
+  const postData = (req, res) => {
+    newWeather = {
+      date: req.body.date,
+      temp: req.body.temp,
+      content: req.body.content,
+      city: req.body.city,
+      description: req.body.description,
+    };
+    projectData = newWeather;
+    res.status(200).send({
+      success: true,
+      message: "All good, thanks for visiting :*",
+      data: newWeather,
+    });
+  };
+  
+  app.post("/projectData", postData);
+
+  
--- website/app.js
+++ website/app.js
@@ -1,5 +1,89 @@
 /* Global Variables */
+const apiKey = 'f72063edaa945717be8af75412558d92';
+const baseURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=metric`;
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// the retrieve weather function capture the user zip, feelings and add internaly our api key and base Url
+const retrieveWeather = () => {
+  const zip = document.getElementById("zip").value;
+  const url = `${baseURL}&zip=${zip}&APPID=${apiKey}`;
+  let feelings = document.getElementById("feelings").value;
+
+// we also need to check if the user forgot to enter the zip or his feelings
+  if (!zip) feelings = "Please enter a zip code";
+  if (!feelings) feelings = "Please Type Your Feelings";
+// after that we will wait for the apu to send us the data then we will store it in the project data object and update the ui with it
+  getWeather(url).then((data) => {
+    postData("/projectData", {
+      date: newDate,
+      temp: Math.round(data.list[0].main.temp),
+      content: feelings,
+      city: data.city.name,
+      description: data.list[0].weather[0].description,
+    }).then(updateUi("/projectData"));
+  });
+};
+
+// in case we got an error we will tell the user to try a correct zip code.
+const getWeather = async (url) => {
+  const res = await fetch(url);
+  if (res.status === 404 || res.status === 400) {
+    document.getElementById("content").innerHTML =
+      "Please write a valid zip code!";
+  }
+  try {
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+};
+
+//this is the post data async funtion 
+const postData = async (url = "", data = {}) => {
+  const res = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await res.json();
+    console.log(newData);
+    return newData;
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+};
+
+// the update Ui function will update the home page with root url
+const updateUi = async (url) => {
+  const request = await fetch(url);
+  try {
+    const allData = await request.json();
+    // we will add the date retrieved from the API to the Date Div
+    document.getElementById("date").innerHTML = `Date: ${allData.date}`;
+    // We will add the temp retrieved from the API to the temp div
+    document.getElementById(
+      "temp"
+    ).innerHTML = `Temperature in C°: ${allData.temp}`;
+    document.getElementById(
+      "content"
+    ).innerHTML = `Content: ${allData.content}`;
+    // we will add the city retrieved from the API to the city field
+    document.getElementById("city").innerHTML = `City: ${allData.city}`;
+    document.getElementById(
+      "description"
+    ).innerHTML = `Description: ${allData.description}`;
+  } catch (err) {
+    console.log(`Error: ${err}`);
+  }
+};
+
+const button = document.getElementById("generate");
+// we will create an event listener on the button that will execute the retrieve Weather function
+button.addEventListener("click", retrieveWeather);
